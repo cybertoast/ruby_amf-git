270f8f5db28c89152e74d74104815e3b90d89a03
warhero <warhero@47e52f69-1a3c-0410-a923-c93d89df8d96>
2007-08-01 20:33:29 +0000

    Changes for development testing.

--- rubyamf_core/vo_mappings.rb
+++ rubyamf_core/vo_mappings.rb
@@ -7,9 +7,8 @@ def VoMappings.vos
   #it get's caught by the "incoming" key, then is mapped_to the class defined in the "map_to"
   #hash. When the VO is returned to the client it is returned as the "outgoing" class.
   vo_maps = []
-  vo_maps << {:incoming => 'com.mypackage.Person', :map_to => 'com.rubyamf.internalpackage.Person', :outgoing => 'com.mypackage.Person'}
-  vo_maps << {:incoming => 'com.cpb.hardbody.User', :map_to => 'com.cpb.hardbody.User', :outgoing => 'com.cpb.hardbody.User'}
   vo_maps << {:incoming => 'test.mypackage.Person', :map_to => 'test.mypackage.Person', :outgoing => 'test.mypackage.Person'}
+  vo_maps << {:incoming => 'test.mypackage.Person', :map_to => 'PersonFF', :outgoing => 'test.mypackage.Person'}
   
   #return vo_maps hash
   vo_maps
