c1dce618f10e818d990cc693d54df93b71d6066c
warhero <warhero@47e52f69-1a3c-0410-a923-c93d89df8d96>
2007-08-26 17:38:01 +0000

    added getActiveRecordFromOpenStruct, for incoming VO usage. Changed all mappings variables to use the new ValueObjects class in configuration.rb Added makeHashFromOpenStruct, used in combination with getActiveRecordFromOpenStruct.

--- rubyamf_core/util/vo_util.rb
+++ rubyamf_core/util/vo_util.rb
@@ -1,9 +1,11 @@
 #Copyright (c) 2007 Aaron Smith (aaron@rubyamf.org) - MIT License
 
 #utility method to get a vo object mapping from the vo_mappings module
-require 'vo_mappings'
+#require 'vo_mappings'
+require 'app/configuration'
 require 'app/request_store'
 require 'exception/rubyamf_exception'
+include RUBYAMF::Configuration
 
 class VoUtil
   
@@ -55,9 +57,22 @@ class VoUtil
     nil
   end
   
+  #make a hash from an open struct
+  def self.makeHashFromOpenStruct(os)
+    hash = {}
+    members = os.marshal_dump.keys.map{|k| k.to_s}
+    members.each do |key|
+      if key == '_explicitType' || key == 'rmembers'
+        next
+      end
+      hash[key] = os.send(:"#{key}")
+    end
+    hash
+  end
+  
   #get a VO definition from the client incoming class
   def self.getVoDefFromIncoming(incom)
-    mappings = VoMappings.vos
+    mappings = ValueObjects.get_vo_mappings
     mappings.each do |map|
       if map[:incoming] == incom
         return map
@@ -69,7 +84,7 @@ class VoUtil
   #get an instance VO from the incoming type
   def self.getVoInstanceFromIncoming(incom)
     begin
-      mappings = VoMappings.vos
+      mappings = ValueObjects.get_vo_mappings
       mappings.each do |map|
         if map[:incoming] == incom
           filepath = map[:map_to].split('.').join('/').to_s + '.rb' #set up filepath from the map_to symbol
@@ -85,7 +100,7 @@ class VoUtil
   
   #get a VO from a ruby mapped VO (a transitional object the map_to key of the map hash)
   def self.getVoDefFromMappedRubyObj(classname)
-    mappings = VoMappings.vos
+    mappings = ValueObjects.get_vo_mappings
     m = '' #TODO - Fix me, why do I need to initialize m here ??
     mappings.each do |map|
       if map[:map_to] == classname
@@ -96,11 +111,11 @@ class VoUtil
     return m
   end
   
+  #get a vo instnace from a mapped ruby object
   def self.getVoInstanceFromMappedRubyObj(classname)
     begin
-      mappings = VoMappings.vos
+      mappings = ValueObjects.get_vo_mappings
       mappings.each do |map|
-        #TODO - change all this to use OpenStruct instead of VO_instance, that way the "incoming" key could be nil and not matter
         if map[:map_to] == classname
           filepath = map[:outgoing].split('.').join('/').to_s + '.rb' #set up filepath from the map_to symbol
           require RUBYAMF_VO + '/' + filepath #require the file
@@ -111,5 +126,16 @@ class VoUtil
       raise RUBYAMFException.new(RUBYAMFException.VO_ERROR, "Tho VO definition #{classname} could not be found")
     end
     nil
+  end
+  
+  #get an active record from an incoming VO openStruct
+  def self.getActiveRecordFromOpenStruct(os)
+    if os._explicitType == nil
+      return nil
+    end
+    classname = os._explicitType
+    hash = self.makeHashFromOpenStruct(os)
+    ar = Object.const_get(classname).new(hash)
+    return ar
   end
 end
