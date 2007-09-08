8925b6f68c94bac8daba0032b60cb3929475faab
warhero <warhero@47e52f69-1a3c-0410-a923-c93d89df8d96>
2007-09-08 19:27:43 +0000

    updated adapters config.

--- services/config/adapters_config.rb
+++ services/config/adapters_config.rb
@@ -1,7 +1,7 @@
 #=>GLOBAL ADAPTER CONFIGURATION
 #=>These shouldn't need to change, just uncomment any others if needed.
 #
-# Adapters are run against your service results. Your results can qualify to be 'adapted' by one of these adapters.
+# Adapters are run against your service results. Your results can qualify to be adapted by one of these adapters.
 # Each adapter must have a 'use_adapter?' and 'run' method defined in it.
 # 'use_adapter?' is used to qualify your results to be run against this adapter
 # 'run' is used to run the results through the adapter, and alters your service result to whatever the adapter chooses.
@@ -28,5 +28,5 @@ Adapters.register('mysql_adapter', 'MysqlAdapter')
 # For example, if you wrapped a result from "u = User.find(:all)" in an array (render :amf => [u])
 # The result inside of the array would be caught as being adaptable, and therefor run against the active record adapter.
 # This is nice if you're wanting to return multiple sets of results in an array.
-# BE AWARE, THIS SLOWS SERIALIZATION DOWN!
+# BUT BEWARE, THIS SLOWS SERIALIZATION DOWN!
 Adapters.deep_adaptations = false
