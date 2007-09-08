e2b1cda346eb194371d16cad5b7e53638199ce16
warhero <warhero@47e52f69-1a3c-0410-a923-c93d89df8d96>
2007-09-08 02:29:43 +0000

    refactored the mysql adapter. Faster.!

--- rubyamf_core/adapters/mysql_adapter.rb
+++ rubyamf_core/adapters/mysql_adapter.rb
@@ -1,7 +1,7 @@
 require 'app/amf'
 include RUBYAMF::AMF
 
-#Adapt a Mysql::Result class into an ASRecordSet
+#Adapt a Mysql::Result class into an array of objects
 class MysqlAdapter
 	
 	def use_adapter?(result)
@@ -11,24 +11,12 @@ class MysqlAdapter
     false
 	end
 	
-	#run the action on an AMFBody#result instance var
-	def run(result)		
-		column_names = Array.new #store the column names
-		fields = result.fetch_fields #get all the fields
-		fields.each do |field|
-			column_names << field.name #push a field into the coumn_names
+	def run(result)
+	  payload = Array.new
+	  result.data_seek(0)
+		while row = result.fetch_hash
+			payload << row
 		end
-		
-		row_count = result.num_rows #get the number of rows in the Mysql::Result
-		initial_data = Array.new #payload holder
-		
-		result.data_seek(0) #Seek the cursor to the beginning of the data
-		while row = result.fetch_row
-			initial_data << row # add a row to the payload
-		end
-				
-		asrecordset = ASRecordset.new(row_count,column_names,initial_data)
-		result = asrecordset
-		return result
+		payload
 	end
 end
