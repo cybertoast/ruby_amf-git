f2710b5b805d671056da1b531c9470e53e978ae6
warhero <warhero@47e52f69-1a3c-0410-a923-c93d89df8d96>
2007-09-10 07:40:22 +0000

    added to_hash method, took out id/id= and changed to attr_accessor.

--- rubyamf_core/util/object.rb
+++ rubyamf_core/util/object.rb
@@ -1,19 +1,22 @@
-require 'app/configuration'
 class Object
-  
   attr_accessor :_explicitType
   attr_accessor :rmembers
+  attr_accessor :id
     
-  def id
-    return @amf_id
-  end
-  
-  def id=(val)
-    @amf_id = val
-  end
-  
   def get_members
     members = obj.instance_variables.map{|mem| mem[1,mem.length]}
     members
+  end
+  
+  def to_hash
+    hash= {}
+    members = self.get_members
+    members.each do |m|
+      hash[m] = self.send(:"#{k}")
+    end
+    if !self.id.nil?
+      hash['id'] = self.id
+    end
+    hash
   end
 end
--- rubyamf_core/util/openstruct.rb
+++ rubyamf_core/util/openstruct.rb
@@ -1,15 +1,19 @@
 class OpenStruct
-  def id  
-    @amf_id
-  end
-  
-  def id=(v)
-    @amf_id = v
-  end
   
   def get_members
     members = self.marshal_dump.keys.map{|k| k.to_s}
     members
   end
   
+  def to_hash
+    hash= {}
+    members = self.get_members
+    members.each do |m|
+      hash[m] = self.send(:"#{k}")
+    end
+    if !self.id.nil?
+      hash['id'] = self.id
+    end
+    hash
+  end
 end
